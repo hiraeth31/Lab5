8b67453dffd5ae19ab797a0de17f72950b88f0b4
hiraeth31 <beautiful_distress@outlook.com>
2025-04-24 20:18:57 +1000

    desperate

---
+++ Lab5/Pages/Articles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab5.Pages
+{
+    public class ArticlesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
