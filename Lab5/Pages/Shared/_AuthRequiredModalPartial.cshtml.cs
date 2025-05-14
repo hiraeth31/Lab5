1db6ec6f33e0976f6e64b37640a91fdbc5c996c1
hiraeth31 <beautiful_distress@outlook.com>
2025-05-14 20:50:39 +1000

    refresh

---
+++ Lab5/Pages/Shared/_AuthRequiredModalPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab5.Pages.Shared
+{
+    public class _AuthRequiredModalPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
